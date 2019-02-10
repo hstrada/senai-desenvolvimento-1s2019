1f5094d564d693e2f75f8d66201a7e53c899d02b
Helena Strada <h.strada@hotmail.com>
2019-02-10 20:52:23 -0200

    up

--- svigufo/2.api-backend/Senai.Svigufo.Api/Senai.Svigufo.Api/Controllers/TiposEventosController.cs
+++ svigufo/2.api-backend/Senai.Svigufo.Api/Senai.Svigufo.Api/Controllers/TiposEventosController.cs
@@ -2,12 +2,14 @@
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Senai.Svigufo.Api.Domains;
 
 namespace Senai.Svigufo.Api.Controllers
 {
     [Produces("application/json")]
     [Route("api/[controller]")]
+    [ApiController]
     public class TiposEventosController : ControllerBase
     {
 
@@ -17,7 +19,7 @@ namespace Senai.Svigufo.Api.Controllers
             ,new TipoEventoDomain { Id = 3, Nome = "Tipo Evento C" }
             ,new TipoEventoDomain { Id = 4, Nome = "Tipo Evento D" }
         };
-        
+
         //[HttpGet]
         //public string Get()
         //{
@@ -41,5 +43,27 @@ namespace Senai.Svigufo.Api.Controllers
             return Ok(evento);
         }
 
+        [HttpPost]
+        public IActionResult Post(TipoEventoDomain tipoEvento) {
+
+            eventos.Add(new TipoEventoDomain() { Id = eventos.Count + 1, Nome = tipoEvento.Nome });
+            return Ok(eventos);
+            //tipoEvento.Id = eventos.Count + 1;
+            //return Ok(tipoEvento);
+        }
+
+        [HttpPut]
+        public IActionResult Put(TipoEventoDomain tipoEvento) {
+            var eventoEncontrado = eventos.Find(x => x.Id == tipoEvento.Id);
+            eventoEncontrado.Nome = tipoEvento.Nome;
+
+            return Ok(eventos);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) {
+            eventos.Remove(eventos.Find(x => x.Id == id));
+            return Ok(eventos);
+        }
     }
 }
