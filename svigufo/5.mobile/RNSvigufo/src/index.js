c91611a26f3e54a77287921fe45c638e1d6cfd85
Helena Strada <h.strada@hotmail.com>
2019-04-23 11:15:00 -0300

    adicionando projeto do react native

---
+++ svigufo/5.mobile/RNSvigufo/src/index.js
@@ -0,0 +1,31 @@
+import {
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator
+} from "react-navigation";
+
+import Main from "./pages/main";
+import Profile from "./pages/profile";
+
+const MainNavigator = createBottomTabNavigator(
+  {
+    Main,
+    Profile
+  },
+  {
+    swipeEnabled: true,
+    tabBarOptions: {
+      showLabel: false,
+      showIcon: true,
+      inactiveBackgroundColor: "#B727FF",
+      activeBackgroundColor: "#B727FF",
+      activeTintColor: "#FFFFFF",
+      inactiveTintColor: "#FFFFFF",
+      style: {
+        height: 50
+      }
+    }
+  }
+);
+
+export default createAppContainer(MainNavigator);
---
+++ svigufo/5.mobile/RNSvigufo/src/pages/profile.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+import { Text, Image, StyleSheet, View } from "react-native";
+
+class Profile extends Component {
+  static navigationOptions = {
+    tabBarIcon: ({ tintColor }) => (
+      <Image
+        source={require("../assets/img/profile.png")}
+        style={styles.tabNavigatorIconProfile}
+      />
+    )
+  };
+
+  render() {
+    return (
+      <View style={styles.main}>
+        <View style={styles.mainHeader}>
+          <View style={styles.mainHeaderRow}>
+            <Image
+              source={require("../assets/img/profile.png")}
+              style={styles.mainHeaderImg}
+            />
+            <Text style={styles.mainHeaderText}>{"Perfil".toUpperCase()}</Text>
+          </View>
+          <View style={styles.mainHeaderLine} />
+        </View>
+        <View style={styles.mainBody} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  tabNavigatorIconProfile: {
+    width: 25,
+    height: 25,
+    // tintColor: "purple"
+    tintColor: "#FFFFFF"
+  },
+  main: {
+    flex: 1,
+    backgroundColor: "#F1F1F1"
+  },
+  mainHeader: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  mainHeaderRow: {
+    flexDirection: "row"
+  },
+  mainHeaderImg: {
+    width: 22,
+    height: 22,
+    tintColor: "#cccccc",
+    marginRight: -9,
+    marginTop: -9
+  },
+  mainHeaderText: {
+    fontSize: 16,
+    letterSpacing: 5,
+    color: "#999999",
+    fontFamily: "OpenSans-Regular"
+  },
+  mainHeaderLine: {
+    width: 170,
+    paddingTop: 10,
+    borderBottomColor: "gray",
+    borderBottomWidth: 0.9
+  },
+  mainBody: {
+    flex: 4
+  }
+});
+
+export default Profile;
